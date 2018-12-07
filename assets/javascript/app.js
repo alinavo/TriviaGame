3b14b19f34449362d5d3e797143bdd87528537d4
alinavo <alinahuynhvo@gmail.com>
2018-12-07 16:32:01 -0500

    Questions created

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+
+
+//questions that make up trivia game
+var triviaQuestions = [{
+    //book one
+    question: "Which two houses does the Sorting Hat have difficulty putting Harry in?",
+    answerList: ["Hufflepuff & Gryffindor", "Gryffindor & Slytherin", "Ravenclaw & Hufflepuff", "Gryffindor & Ravenclaw"],
+    answer: 2
+}, {
+    //book two
+    question: "Which Hufflepuff student does the serpent stare down during the duel between Malfoy and Harry?",
+    answerList: ["Justin Finch-Fletchley", "Hannah Abbott", "Susan Bones", "Zacharias Smith"],
+    answer: 1
+}, {
+    //book three
+    question: "Which one of the Marauder’s is Sirius Black?",
+    answerList: ["Moony", "Wormtail", "Padfoot", "Prongs"],
+    answer: 3
+    }, {
+    //book four
+    question: "Who put Harry’s name in the Goblet of Fire?",
+    answerList: ["Igor Kararov", "Barty Crouch Jr.", "Madeye Moody", "Severus Snape"],
+    answer: 2
+}, {
+    //book five
+    question: "What are the creatures Harry and Luna can see but no other students?",
+    answerList: ["Dementors", "Blast-Ended Skrewts", "Nargles", "Thestrals"],
+    answer: 4
+}, {
+    //book six
+    question: "What scent does Hermione NOT describe when sniffing the love potion <i>Amortentia</i> ?",
+    answerList: ["Freshly mown grass", "Spearmint toothpaste", "Roses", "Parchment"],
+    answer: 3
+}, {
+    //book seven
+    question: "Which magical object is NOT one of the three Deathly Hallows",
+    answerList: ["Time-Turner", "Elder Wand", "Invisibility Cloak", "Resurrection Stone"],
+    answer: 1
+}, {
+    //extra Q
+    question: "Who was Headmaster of Hogwarts before Albus Dumbledore",
+    answerList: ["Cornelius Fudge", "Rufus Scrimgeour", "Phineas Nigellus", "Armando Dippet"],
+    answer: 4
+}, {
+    //extra Q
+    question: "Which ghost died the first time the Chamber of Secrets was opened?",
+    answerList: ["Bloody Baron", "Sir Nicholas", "Moaning Myrtle", "Helena Hufflepuff"],
+    answer: 3
+}, {
+    //extra Q
+    question: "How many children do Molly and Arthur Weasley have?",
+    answerList: ["5", "6", "7", "8"],
+    answer: 3
+}];
+
+
+
+
+
+
+
